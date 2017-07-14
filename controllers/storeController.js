31667d5f094479c1cf820df9bf38ef011bd2ecc4
wizard2014 <wizard2014@ya.ru>
2017-07-14 20:37:46 +0300

    add some form changes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -32,8 +32,25 @@ exports.createStore = async (req, res) => {
   res.redirect(`/store/${store.slug}`);
 };
 
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return new store instead of old one
+    runValidators: true
+  }).exec();
+
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View store</a>`);
+
+  res.redirect(`/stores/${store._id}/edit`);
+};
+
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
 
   res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+
+  res.render('editStore', { title: `Edit ${store.name}`, store });
 };
