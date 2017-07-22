d54cb0f3142bcf0e60c40db2478176ba41141f3a
wizard2014 <wizard2014@ya.ru>
2017-07-22 11:38:13 +0300

    add user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate('local', {
   successRedirect: '/',
   successFlash: 'You now logged in!'
 });
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are logged out!');
+  res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+    return;
+  }
+
+  req.flash('error', 'Oops! You must be logged in!');
+  res.redirect('/login');
+};
--- models/User.js
+++ models/User.js
@@ -20,6 +20,12 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+userSchema.virtual('gravatar').get(function () {
+  const hash = md5(this.email);
+
+  return `https://gravatar.com/avatar/${hash}?s=20`;
+});
+
 userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 userSchema.plugin(mongodbErrorHandler);
 
